858553e36908ca6d52b1066b226c49818f22279c
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-01-11 00:56:41 +0530

    menubar onclick added

--- components/independent/menubar_option.js
+++ components/independent/menubar_option.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-function MenuBarOption({ icon, label }) {
+function MenuBarOption({ icon, label, onclick }) {
     return (
         <>
-            <div className="flex flex-col shrink-0 justify-center items-center hover:bg-[#e8e9eb] h-[120px] w-[120px] md:h-[180px] md:w-[180px] border rounded-md cursor-pointer mb-2">
+            <div className="flex flex-col shrink-0 justify-center items-center hover:bg-[#e8e9eb] h-[120px] w-[120px] md:h-[180px] md:w-[180px] border rounded-md cursor-pointer mb-2" onClick={onclick}>
                 <Image
                     src={icon}
                     className="h-[30px] w-[30px] md:h-[50px] md:w-[50px]"
--- components/menu_bar.js
+++ components/menu_bar.js
@@ -2,12 +2,14 @@ import MenuBarOption from "./independent/menubar_option";
 import profilePicIcon from "../assets/img/profile-pic-icon.png";
 import uploadResumeIcon from "../assets/img/upload-resume-icon.png";
 import copyPortfolioIcon from "../assets/img/copy-portfolio-link-icon.png";
+import domainManageIcon from "../assets/img/domain-manage-icon.png";
 
 function MenuBar() {
     return (
         <>
             <div className="flex flex-row flex-nowrap gap-x-4 md:gap-x-8 overflow-x-auto mt-6 md:mt-10">
                 <MenuBarOption icon={profilePicIcon} label="Edit Profile" />
+                <MenuBarOption icon={domainManageIcon} label="Domain Management" />
                 <MenuBarOption icon={uploadResumeIcon} label="Upload Resume" />
                 <MenuBarOption icon={copyPortfolioIcon} label="Copy Portfolio" />
             </div>
