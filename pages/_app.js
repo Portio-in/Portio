cf67a415ed309b618f8d09919dcb01200ce07def
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-02-03 19:10:14 +0530

    tawk.to integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <>
+        <Component {...pageProps} />
+
+        <TawkMessengerReact
+          propertyId="63dd0b264742512879114eda"
+          widgetId="1gobn74nv"
+      />
+  </>)
 }
