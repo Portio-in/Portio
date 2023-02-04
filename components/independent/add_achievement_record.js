a6b54e86bac2f8163efbc5ad7175ef49cbf855cf
cAmartya <amartya.3654@gmail.com>
2023-02-04 09:59:50 +0530

    check add_achievement_record

--- components/independent/add_achievement_record.js
+++ components/independent/add_achievement_record.js
@@ -9,6 +9,14 @@ function AddAchievementRecord({onClick}) {
                     <FontAwesomeIcon icon={faPlus} />
                 </div>
             </div>
+            {/* <div className="flex flex-col shrink-0 justify-center items-center hover:bg-[#e8e9eb] h-auto w-auto border rounded-md mb-2 px-8 py-8 cursor-pointer" onClick={onClick}>
+                <div className="flex justify-center items-center h-[40px] w-[40px] md:h-[50px] md:w-[50px] text-white text-sm md:text-xl bg-brand rounded-full">
+                    <FontAwesomeIcon icon={faPlus} />
+                </div>
+                <p className="font-medium px-5 mt-4 md:mt-7 text-sm md:text-base text-center">
+                    Add Achievement
+                </p>
+            </div> */}
         </>
     );
 }
