0f4b1f44d34b1eaf4c7b97f69eb8a1b2dd9087d7
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-02-06 21:40:11 +0530

    privacy policy added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites(){
+  	return [
+  	  {
+        "source": "/privacy-policy",
+        "destination": "/html/privacy.html"
+  	  }
+  	]
+  },
   images: {
     remotePatterns: [
       {
