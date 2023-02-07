2194efbb9e0179d949f142fd1ce143f78f874dc3
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-02-08 00:09:12 +0530

    google analytics tag added

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -11,6 +12,19 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Google tag (gtag.js)  */}
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-JE2RN358R0'
+          strategy='afterInteractive'
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JE2RN358R0');
+            `}
+        </Script>
       </body>
     </Html>
   )
